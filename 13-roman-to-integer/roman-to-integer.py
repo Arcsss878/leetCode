23d12f8a2c1fce89032b5e8e7c87356f4619f3ea
Arcsss878 <stevenhans171@gmail.com>
2026-05-15 21:58:29 +0700

    Time: 8 ms (64.86%) | Memory: 12.4 MB (21.98%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def romanToInt(self, s):
-        rom_val = {
+        val = {
             'I' : 1,
             'V' : 5,
             'X' : 10,
@@ -9,12 +9,14 @@ class Solution(object):
             'D' : 500,
             'M' : 1000
         }
-        res = 0
-        for i in range(len(s) - 1):
-            if rom_val[s[i]] < rom_val[s[i+1]]:
-                res -= rom_val[s[i]]
+        total = 0
+        prev = 0
+        for char in reversed(s):
+            cur = val[char]
+            if cur < prev:
+                total -= cur
             else:
-                res += rom_val[s[i]]
-        
-        res += rom_val[s[-1]]
-        return res
+                total += cur
+            prev = cur
+
+        return total
