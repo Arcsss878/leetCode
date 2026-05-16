8601be985a6bee8381450c08e77ff335d48a69ec
Arcsss878 <stevenhans171@gmail.com>
2026-05-16 17:06:56 +0700

    Time: 14 ms (90.14%) | Memory: 12.6 MB (67.30%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,5 +1,19 @@
 class Solution(object):
     def isAnagram(self, s, t):
         #pakai sorted(), nanti dirapiin terus di bandingin
-        return sorted(s) == sorted (t)
-        
+        #return sorted(s) == sorted(t)
+
+        #cara lebih efektif
+        if len(s) != len(t): #langsung salah klo panjang beda
+            return False
+        count = {}
+        for char in s: #char ni nanti mbaca karakter 1 1
+            #get():cek dict ada isi ?kalo ga ada get defnya 0
+            count[char] = count.get(char,0) + 1                  
+        for char in t:
+            if char not in count: #langsung salah kalo ga ada
+                return False
+            count[char] -= 1 #kurangi 1 1 sampe habis kalo emang anagam
+            if count[char] < 0:
+                return False #kalo negatif, t punya lebih banyak char dari s
+        return True
