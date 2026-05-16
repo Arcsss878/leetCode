8ebc545a5c9427f6537cb15bd52e6536e730f6ec
Arcsss878 <stevenhans171@gmail.com>
2026-05-16 16:45:26 +0700

    Time: 12 ms (84.87%) | Memory: 25.8 MB (56.50%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,9 +1,13 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        seen = {}
-        for val in nums:
-            if val in seen:
-                return True
-            seen[val] = True
-        return False
+        #seen = {}
+        #for val in nums:
+        #    if val in seen:
+        #        return True
+        #    seen[val] = True
+        #return False
+
+        #pakai set(), set ini langsung ilangin duplikat 
+        #jadi kalo ukuran beda pasti ada duplikat
+        return len(nums) != len(set(nums))
         
