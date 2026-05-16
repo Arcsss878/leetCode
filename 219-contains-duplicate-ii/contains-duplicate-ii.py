78d76ee9a52a0f76b460e6d5126d37536ba81e5f
Arcsss878 <stevenhans171@gmail.com>
2026-05-16 17:37:35 +0700

    Time: 32 ms (74.56%) | Memory: 23.9 MB (84.26%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -1,8 +1,13 @@
 class Solution(object):
     def containsNearbyDuplicate(self, nums, k):
+        # ni isine angka yang uda diliat sama index e dimana
         seen = {}
+        # pake enumerate karena butuh sama indexnya juga
         for i, num in enumerate(nums):
+            # ngecek kalo num ada di seen dan index num sekarang - index num sebelum <= k
+            # bakal return true karena syarat true itu kalo duplikatnya setidaknya sejauh k
             if num in seen and i - seen[num] <= k:
                 return True
+            #ini cuman memperbaharui index num trakhir diliat
             seen[num] = i
         return False
