e63d9554736da25da49e9335b7e006dfbdcb4ea7
Arcsss878 <stevenhans171@gmail.com>
2026-05-18 20:42:56 +0700

    Time: 7 ms (50.44%) | Memory: 12.8 MB (83.38%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,12 +1,18 @@
 class Solution(object):
     def twoSum(self, numbers, target):
+        #buat pointer buat penanda ujung kanan kiri
         left = 0
         right = len(numbers) - 1
+        #selama kiri belum melewati kanan lanjut
         while left < right:
+            #jumlah angka ujung kanan sama kiri
             cur_sum = numbers[left] + numbers[right]
+            #kalo sama langsung selesai
             if cur_sum == target:
                 return [left + 1, right + 1]
+            #kalo kurang dari target berarti angka kirinya kurang besar jadi kita geser
             elif cur_sum < target:
                 left += 1
+            #ini logicnya sama kayak kurang dari cuman kebalikan karena cari yang besar
             else:
                 right -= 1
