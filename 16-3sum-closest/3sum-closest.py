b736aa7c71ddebf1dda9bd72e587658f65d0c875
Arcsss878 <stevenhans171@gmail.com>
2026-05-19 20:23:27 +0700

    Time: 698 ms (5.03%) | Memory: 12.4 MB (78.46%) - LeetSync

--- 16-3sum-closest/3sum-closest.py
+++ 16-3sum-closest/3sum-closest.py
@@ -1,21 +1,33 @@
 class Solution(object):
     def threeSumClosest(self, nums, target):
+        #init close dulu buat pembanding
         nums.sort()
         close = nums[0] + nums[1] + nums[2]
-
+        #for nya kayak 3 sum biasa
         for i in range(len(nums) - 2):
+            # Early break: if i repeats, skip (optional, but can speed up)
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            #init left right kayak 3 sum
             left = i + 1
             right = len(nums) - 1
 
             while left < right:
+                #itung jumlah buat pembanding utama
                 cur = nums[i] + nums[left] + nums[right]
                 
+                #bandingin selisih target sama cur dan close kalo kecilan cur ya close ganti cur
                 if abs(cur - target) < abs(close - target):
                     close = cur
 
-                if cur > target:
-                    right -= 1      
-                else:
+                #kayak 2 pointer pada umumnya (while buat skipping duplicates for speed di three sum sebelum ada)
+                if cur < target:
                     left += 1
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1      
+                else:
+                    right -= 1
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
         return close
 
