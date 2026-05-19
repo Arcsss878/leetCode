4557275dc5c4aedcb83560ae5a63e33785e64fb2
Arcsss878 <stevenhans171@gmail.com>
2026-05-19 19:32:36 +0700

    Time: 87 ms (87.78%) | Memory: 20.6 MB (63.33%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,12 +1,15 @@
 class Solution(object):
     def maxArea(self, height):
+        #pake two pointer sama siapin buat simpen terbesar
         left = 0
         right = len(height) - 1
         largest = 0
-        while left != right:
+
+        while left < right:
             luas = min(height[right], height[left]) * (right-left)
             if luas > largest:
                 largest = luas
+
             if height[right] < height[left]:
                 right -=1
             else:
