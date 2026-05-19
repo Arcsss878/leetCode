d97abec154401dfe8bd46ef8a2f8e78839cc4040
Arcsss878 <stevenhans171@gmail.com>
2026-05-19 19:45:12 +0700

    Time: 98 ms (81.20%) | Memory: 20.7 MB (24.94%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -5,14 +5,34 @@ class Solution(object):
         right = len(height) - 1
         largest = 0
 
+        #buat loop selama left != right
         while left < right:
+            #cari luas (tinggi terpendek dikali sama selisih index buat luas)
             luas = min(height[right], height[left]) * (right-left)
+            #kalo lebih besar luasnnya langsung di catet
             if luas > largest:
                 largest = luas
 
+            #gerakin pointer yang lebih kecil sapa tau lebih baik
             if height[right] < height[left]:
-                right -=1
+                right -= 1
             else:
-                left +=1
+                left += 1
         return largest
         
+
+        #better solution(di ringkas aja sih, sama tambah while dalem if buat skip tinggi yang < atau = dengan yang udah di komputasiin)
+        while left < right:
+            luas = min(height[left], height[right]) * (right - left)
+            largest = max(largest, luas)
+    
+            if height[left] < height[right]:
+                # Skip all left heights that are <= current left height
+                current_left = height[left]
+                while left < right and height[left] <= current_left:
+                    eft += 1
+            else:
+                # Skip all right heights that are <= current right height
+                current_right = height[right]
+                while left < right and height[right] <= current_right:
+                     right -= 1
