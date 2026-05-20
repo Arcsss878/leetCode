ee85ad41ba967a9d75636e49d7302d2b34101082
Arcsss878 <stevenhans171@gmail.com>
2026-05-20 20:23:20 +0700

    Time: 0 ms (100.00%) | Memory: 12.4 MB (66.27%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -1,9 +1,14 @@
 class Solution(object):
     def intersection(self, nums1, nums2):
+        #cara cepet
+        return list(set(nums1) & set(nums2))
+        #cara normal
         seen = {}
         hasil = []
+        #set dictionary dulu pake satu array
         for num in nums1:
             seen[num] = True
+        #di loopnya ini cuman ngecek buat apakah num udah ada di seen tapi belum dimasukin ke hasil
         for num in nums2:
             if num in seen and num not in hasil:
                 hasil.append(num)
