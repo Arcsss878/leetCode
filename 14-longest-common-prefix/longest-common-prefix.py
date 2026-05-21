cfb9e2c8c529771135e53d4034e134107f6752aa
Arcsss878 <stevenhans171@gmail.com>
2026-05-21 19:37:47 +0700

    Time: 0 ms (100.00%) | Memory: 12.5 MB (33.52%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,13 +1,17 @@
 class Solution(object):
     def longestCommonPrefix(self, strs):
+        #cek kalo listnya kosong
         if not strs:
             return ""
         # Start with the first string as the prefix
         prefix = strs[0]
         for s in strs[1:]:
             # Reduce prefix until it matches the start of s
+            #s.startswith(prefix) is a built-in string method that returns True if string s starts with the characters in prefix
+            #jadi ni selama s ga start sama character di prefix bakal kena loop misal "flow" start with "flower" kan enggak sampek kita pretelin blakangnya prefix 1 1
             while not s.startswith(prefix):
                 prefix = prefix[:-1]  # chop off last character
+                #kalo prefix dipretelin sampe kosong berarti ya ga ada jadi return
                 if not prefix:
                     return ""
         return prefix
