510a7c9e2d40b492641122efd470a4e7e212ddfe
Arcsss878 <stevenhans171@gmail.com>
2026-05-29 21:25:36 +0700

    Time: 4 ms (91.74%) | Memory: 12.5 MB (22.94%) - LeetSync

--- 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.py
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.py
@@ -1,15 +1,22 @@
 class Solution(object):
     def minElement(self, nums):
+        #cara efisien
         min_sum = float('inf')
         for num in nums:
+            #sbuat simpen angka baru
             s = 0
             n = num
+            #selama n bukan nol (n ini nanti nol kalo udah digit trakhir // 10 pasti jadi nol)
             while n:
+                #modulo 10 ini ambil digit trakhir, // 10 buat ngapus digit paling blakang
                 s += n % 10
                 n //= 10
+            #kalo sumnya lebih kecil dari min di jadiin nilai terkecil
             if s < min_sum:
                 min_sum = s
         return min_sum
+
+        #cara logic
         #declare yang terkecil dulu, kasik infinite biar semuanya lebih kecil
         smallest = float('inf')
         #iterasi setiap isinya
