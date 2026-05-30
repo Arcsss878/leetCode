dfc74ca2a9f16747a27e9682e3456b9cd1d981e1
Arcsss878 <stevenhans171@gmail.com>
2026-05-30 18:26:34 +0700

    Time: 4 ms (48.33%) | Memory: 13.3 MB (67.77%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,9 +1,16 @@
 class Solution(object):
     def missingNumber(self, nums):
+        #kita pake logic XOR disini buat cancel out angka kembar
+        #di kasus ini angkanya urut dari satu sampe n cuman angka hilangnya di ganti nol, angkanya ini nanti bisa di cek dengan xor dari 1 - n nanti saling cancel out dan sisa angka yang hilang
+
+        #buat res untuk menyimpan hasil XOR (XOR ini kalo misal angka yang sama bakal return 0 )
         res = 0
+        #kita fornya pake indexing aja biar bisa sekalian ngecek
         for i in range(len(nums)):
+            #disini kita langsung aja XOR dari 1 sama isi nums[1] sampe n untuk keduanya
             res ^= i+1
             res ^= nums[i]
+        #kembalikin hasilnya
         return res
         
         
