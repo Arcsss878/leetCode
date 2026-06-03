96eaf4c17e51c9466451f4dd68935b1bac90fea2
Arcsss878 <stevenhans171@gmail.com>
2026-06-03 18:55:44 +0700

    Time: 0 ms (100.00%) | Memory: 12.7 MB (33.43%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -1,7 +1,9 @@
 class Solution(object):
     def runningSum(self, nums):
+        #buat penyimpan hasil dan akumulasi angka
         curr_sum = 0
         r = []
+        #tambah sum tiap putaran dan tambah di array
         for i in nums:
             curr_sum += i
             r.append(curr_sum)
