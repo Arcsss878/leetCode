1e5a453d4936d7fbccbf76da3437927db985ca29
Arcsss878 <stevenhans171@gmail.com>
2026-06-03 18:44:57 +0700

    Time: 0 ms (100.00%) | Memory: 12.4 MB (51.85%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -1,9 +1,12 @@
 class Solution(object):
     def kidsWithCandies(self, candies, extraCandies):
+        #buat most buat nampung brapa permen terbanya, r buat result
         most = 0
         r = []
+        #cari brapa candy terbanyaknya
         for i in candies:
             most = max(most, i)
+        #bandingin candy terbanyak smaa candy dipunya + extra kalo >= true kalo ga false
         for i in candies:
             candy = i + extraCandies
             if  candy >= most:
