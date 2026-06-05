a058b596772323fff30dc1ea5743dc458789ec24
Arcsss878 <stevenhans171@gmail.com>
2026-06-05 21:30:46 +0700

    Time: 0 ms (100.00%) | Memory: 12.3 MB (61.27%) - LeetSync

--- 1791-richest-customer-wealth/richest-customer-wealth.py
+++ 1791-richest-customer-wealth/richest-customer-wealth.py
@@ -1,9 +1,21 @@
 class Solution(object):
     def maximumWealth(self, accounts):
+        #buat penyimpan untuk maxnya
         max_wealth = 0
+        #loop per customer
+        # First iteration: customer = [1, 2, 3]
+        # Second iteration: customer = [3, 2, 1]
         for customer in accounts:
+            #jumlahin customer dalam 1 row itu
             c_sum = sum(customer)
             if c_sum > max_wealth:
                 max_wealth = c_sum
         return max_wealth
+
+        #other way
+        return max(sum(customer) for customer in accounts)
+
+        #note 
+        rows = len(accounts)# number of customers
+        cols = len(accounts[0])# number of banks per customer (assuming non-empty)
         
