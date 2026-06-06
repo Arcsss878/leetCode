7f2b50305fb3e0ba9114bea9bf38fd41436cf496
Arcsss878 <stevenhans171@gmail.com>
2026-06-06 19:56:15 +0700

    Time: 8 ms (69.40%) | Memory: 13.3 MB (80.37%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,17 +1,21 @@
 class Solution(object):
     def isIsomorphic(self, s, t):
+        #kalau panjangnya beda langsung salah
         if len(s) != len(t):
             return false
+        #buat mapping masing"(untuk mengetahui masing masing huruf menggantikan apa)
         mapSt = {}
         mapTs = {}
+        #loop sebanyak huruf s/t
         for i in range(len(s)):
+            #buat penyimpanan(biar lbh gampang di reuse)
             c1 = s[i]
             c2 = t[i]
 
-            # Check forward mapping
+            # Check forward mapping, kalo hurufnya udah ada di mapnya dan isinya ga sama kayak c2 berarti salah
             if c1 in mapSt and mapSt[c1] != c2:
                 return False
-            # Check backward mapping
+            # Check backward mapping, aturan diatas berlaku buat backwarnya juga
             if c2 in mapTs and mapTs[c2] != c1:
                 return False
 
